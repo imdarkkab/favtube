9b771f613c64c583ca89a0e2bfd31e9d381ec1ba
imdarkkab <imdarkkab@gmail.com>
2014-07-25 18:47:50 +0700

    Initial

---
+++ FavTube/src/com/example/favtube/dummy/DummyContent.java
@@ -0,0 +1,56 @@
+package com.example.favtube.dummy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ * <p>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class DummyContent {
+
+	/**
+	 * An array of sample (dummy) items.
+	 */
+	public static List<DummyItem> ITEMS = new ArrayList<DummyItem>();
+
+	/**
+	 * A map of sample (dummy) items, by ID.
+	 */
+	public static Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
+
+	static {
+		// Add 3 sample items.
+		for(int i=1;i<=100;i++){
+			addItem(new DummyItem(""+i, "Item "+i));			
+		}
+
+	}
+
+	private static void addItem(DummyItem item) {
+		ITEMS.add(item);
+		ITEM_MAP.put(item.id, item);
+	}
+
+	/**
+	 * A dummy item representing a piece of content.
+	 */
+	public static class DummyItem {
+		public String id;
+		public String content;
+
+		public DummyItem(String id, String content) {
+			this.id = id;
+			this.content = content;
+		}
+
+		@Override
+		public String toString() {
+			return content;
+		}
+	}
+}
